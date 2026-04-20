23ff25b83a78734b0eb19849d5e82b41b5bff01e
kim gwanhee <rhksgml55@gmail.com>
2026-04-20 11:36:43 +0900

    chore: upstream/dev conflict 해결

--- src/main/java/com/sprint/mission/findex/domain/syncjob/service/SyncJobService.java
+++ src/main/java/com/sprint/mission/findex/domain/syncjob/service/SyncJobService.java
@@ -10,7 +10,7 @@ import com.sprint.mission.findex.domain.indexinfo.repository.IndexInfoRepository
 import com.sprint.mission.findex.domain.syncclient.client.KrxOpenApiClient;
 import com.sprint.mission.findex.domain.syncclient.dto.IndexDataApiResponse;
 import com.sprint.mission.findex.domain.syncjob.dto.SyncJobResponse;
-import com.sprint.mission.findex.domain.syncjob.dto.SyncJobQueryCondition;
+import com.sprint.mission.findex.domain.syncjob.dto.SyncJobSearchCondition;
 import com.sprint.mission.findex.domain.syncjob.entity.JobResult;
 import com.sprint.mission.findex.domain.syncjob.entity.JobType;
 import com.sprint.mission.findex.domain.syncjob.entity.SyncJob;
@@ -134,7 +134,7 @@ public class SyncJobService {
 
   @Transactional(readOnly = true)
   public CursorPageResponse<SyncJobResponse> getSyncJobHistory(
-      SyncJobQueryCondition condition,
+      SyncJobSearchCondition condition,
       String cursor,
       UUID idAfter,
       String sortField,
